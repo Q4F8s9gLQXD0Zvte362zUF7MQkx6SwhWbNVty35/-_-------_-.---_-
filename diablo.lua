8690d05e4b1dbc96a20fcb796ee5152aa6c6c68d
Q4F8s9gLQXD0Zvte362zUF7MQkx6SwhWbNVty35 <108615685+Q4F8s9gLQXD0Zvte362zUF7MQkx6SwhWbNVty35@users.noreply.github.com>
2022-07-30 15:25:25 +0200

    Add files via upload

---
+++ diablo.lua
@@ -0,0 +1,411 @@
+print("if u see this u are dumb :>")
+ wait(1)
+    game.StarterGui:SetCore("SendNotification", {
+      Title = "Fucking";
+      Text = "Zenokei Ass";
+      Icon = "http://www.roblox.com/asset/?id=8864247034";
+      Duration = "2";
+      Button1 = "im gay";
+      Button2 = "im so gay";
+   })
+   wait(3)
+   game.StarterGui:SetCore("SendNotification", {
+     Title = "Mikami Fatass";
+     Text = "1.40 cm gremblin!";
+     Icon = "http://www.roblox.com/asset/?id=1995010005";
+     Duration = "2";
+   })
+
+
+   local IsuLib = loadstring(game:HttpGet("https://nigger.identityhub.tk/ui/library.lua"))()
+
+ local Window = IsuLib:Create({
+     Name = "Furious Panda | V0.4",
+     StartupSound = {
+         Toggle = true,
+         SoundID = "rbxassetid://6958727243", -- Win 11 Startup Sound
+         TimePosition = 1
+     }
+ })
+
+ local PandasUwU = Window:Tab('Grind')
+ local Example = Window:Tab('Misc')
+ local OwO = Window:Tab("Pvp Options")
+ local Pene = Window:Tab('AutoStyle')
+ local Wuapo = Window:Tab('AutoClan')
+ local Sexo = Window:Tab('AutoBuy')
+ local anal = Window:Tab('Premium')
+
+ PandasUwU:Toggle("Auto Equip Push Ups", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+  game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Pushup Training"))
+    end
+ end)
+
+
+ PandasUwU:Toggle("Auto Pulls Ups", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+
+ fireclickdetector(game:GetService("Workspace").TrainingStations.Model.ClickDetector)
+    end
+ end)
+
+
+
+ PandasUwU:Toggle("Auto Clicker", function(Toggle)
+
+  local vu = game:GetService("VirtualUser")
+  getgenv().XD = Toggle
+     while getgenv().XD == true do
+     task.wait()
+ 
+  vu:Button1Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+  wait(0.1)
+  vu:Button1Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    end
+  end)
+  
+  Wuapo:Button('Sung', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Sung.ClickDetector)
+  end)
+
+  Wuapo:Button('Raijin', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Raijin.ClickDetector)
+  end)
+
+  Wuapo:Button('Fujin', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Fujin.ClickDetector)
+  end)
+
+  Wuapo:Button('Ryu', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Ryu.ClickDetector)
+  end)
+
+  Wuapo:Button('Kagutsuchi', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Kagutsuchi.ClickDetector)
+  end)
+  
+  Wuapo:Button('Gyakusatsu', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Gyakusatsu.ClickDetector)
+  end)
+  
+  Wuapo:Button('SonGoku', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Songoku.ClickDetector)
+  end)
+
+  Wuapo:Button('Advance', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Advance.ClickDetector)
+  end)
+
+  Wuapo:Button('Ohma Advance', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Ohma.ClickDetector)
+  end)
+
+  Pene:Button('Formless', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Formless.ClickDetector)
+  end)
+
+  Pene:Button('Hitman', function()
+    fireclickdetector(game:GetService("Workspace").SpecialTrainers.Hitman.ClickDetector)
+  end)
+
+  Pene:Button('BaGuaZhang', function()
+    fireclickdetector(game:GetService("Workspace").Trainers.BaGuaZhang.ClickDetector)
+  end)
+  Sexo:Toggle("Auto Ramen", function(Toggle)
+    getgenv().XD = Toggle
+       while getgenv().XD == true do
+       task.wait()
+       fireclickdetector(game:GetService("Workspace").Items["7B343766-D4F5-4BEC-84BD-086C44B436DC"].ClickDetector)
+       end
+       end)
+
+       Sexo:Toggle("Auto Chicken", function(Toggle)
+        getgenv().XD = Toggle
+           while getgenv().XD == true do
+           task.wait()
+           fireclickdetector(game:GetService("Workspace").Items["163FEB3C-D538-4BAD-B023-46E95890B183"].ClickDetector)
+           end
+          end)
+          Sexo:Toggle("Auto Choco", function(Toggle)
+            getgenv().XD = Toggle
+               while getgenv().XD == true do
+               task.wait()
+               fireclickdetector(game:GetService("Workspace").Items["80A9903E-C8D3-46B4-849C-762AAE9D9076"].ClickDetector)
+               end
+              end)
+ PandasUwU:Toggle("Auto Dumbbell 20KG", function(Toggle)
+   getgenv().XD = Toggle
+     while getgenv().XD == true do
+     task.wait()
+   game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("20 KG Dumbbell"))
+     end
+ end)
+
+ Sexo:Toggle("Auto EAT chicken", function(Toggle)
+  getgenv().XD = Toggle
+  while getgenv().XD == true do
+  task.wait()
+                              wait(0.1)
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Chicken"))
+                              wait(0.2)
+                              game.Players.LocalPlayer.Character:FindFirstChild("Chicken"):Activate()
+                              wait(30)
+  end
+ end)
+
+ Sexo:Toggle("Auto EAT choco", function(Toggle)
+  getgenv().XD = Toggle
+  while getgenv().XD == true do
+  task.wait()
+                              wait(0.1)
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Chocolate Shake"))
+                              wait(0.2)
+                              game.Players.LocalPlayer.Character:FindFirstChild("Chocolate Shake"):Activate()
+                              wait(30)
+  end
+ end)
+
+ Sexo:Toggle("Auto EAT Ramen", function(Toggle)
+  getgenv().XD = Toggle
+  while getgenv().XD == true do
+  task.wait()
+                              wait(0.1)
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Ramen"))
+                              wait(0.2)
+                              game.Players.LocalPlayer.Character:FindFirstChild("Ramen"):Activate()
+                              wait(1800)
+  end
+ end)
+
+ PandasUwU:Toggle("Auto Dumbbell 50KG", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+  game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("50KG KG Dumbbell"))
+    end
+ end)
+
+ PandasUwU:Toggle("Auto Dumbbell 100KG", function(Toggle)
+   getgenv().XD = Toggle
+     while getgenv().XD == true do
+     task.wait()
+   game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("100 KG Dumbbell"))
+     end
+ end)
+
+
+
+ PandasUwU:Toggle("Auto Dumbbell 200KG", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+  game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("200 KG Dumbbell"))
+    end
+ end)
+
+ PandasUwU:Toggle("Auto Squats", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+  game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Squat Training"))
+    end
+ end)
+
+ PandasUwU:Toggle("Auto Situp", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+  game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Situp Training"))
+    end
+end)
+
+ OwO:Toggle("Change Pov", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+
+ local FOV = 120
+ game.Workspace.Camera.FieldOfView = FOV
+    end
+  end)
+
+  OwO:Button('Fps Boost', function()
+  local decalsyeeted = true -- Leaving this on makes games look shitty but the fps goes up by at least 20.
+ local g = game
+ local w = g.Workspace
+ local l = g.Lighting
+ local t = w.Terrain
+ t.WaterWaveSize = 0
+ t.WaterWaveSpeed = 0
+ t.WaterReflectance = 0
+ t.WaterTransparency = 0
+ l.GlobalShadows = false
+ l.FogEnd = 9e9
+ l.Brightness = 0
+ settings().Rendering.QualityLevel = "Level01"
+ for i, v in pairs(g:GetDescendants()) do
+     if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+         v.Material = "Plastic"
+         v.Reflectance = 0
+     elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+         v.Transparency = 1
+     elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+         v.Lifetime = NumberRange.new(0)
+     elseif v:IsA("Explosion") then
+         v.BlastPressure = 1
+         v.BlastRadius = 1
+     elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") then
+         v.Enabled = false
+     elseif v:IsA("MeshPart") then
+         v.Material = "Plastic"
+         v.Reflectance = 0
+         v.TextureID = 10385902758728957
+     end
+ end
+ for i, e in pairs(l:GetChildren()) do
+     if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+         e.Enabled = false
+     end
+ end
+ end)
+
+ Example:Button('By FuriousPanda', function()
+  print("Furious Sexy")
+ end)
+ 
+ local groupID = 12195311
+
+
+ local plr = game.Players.LocalPlayer
+ repeat wait() until plr.Character
+ 
+ if plr:IsInGroup(groupID) then
+  anal:Button('Remove animations for Damian', function()
+  game.Workspace.Chars.damianPROBLOX.Humanoid.Animator:Destroy()
+  end)
+
+  anal:Button('Remove animations for therockof', function()
+    game.Workspace.Chars.therockof.Humanoid.Animator:Destroy()
+    end)
+    anal:Button('Remove animations for narr', function()
+      game.Workspace.Chars.naruto144uzimaki.Humanoid.Animator:Destroy()
+      end)
+      anal:Button('Remove animations for Scket Not fujin', function()
+        game.Workspace.Chars.Scket666.Humanoid.Animator:Destroy()
+        end)
+
+        anal:Button('Remove animations for bilal', function()
+          game.Workspace.Chars.LamaBilal.Humanoid.Animator:Destroy()
+          end)
+
+          anal:Button('Remove animations for dani', function()
+            game.Workspace.Chars.daniloelamo.Humanoid.Animator:Destroy()
+            end)
+
+            anal:Button('Remove animations for Scket fujin', function()
+              game.Workspace.Chars.Scketzz.Humanoid.Animator:Destroy()
+              end)
+
+            
+          
+      
+    
+ else
+     print ("U aren't Ultra Premium :o")
+ end
+
+
+ local LocalPlayer = game.Players.LocalPlayer
+ local Players = game:GetService("Players")
+
+ Example:Button('Anti Staff', function()
+ Players.PlayerAdded:Connect(function(player)
+   if player.Name == "Avazic" or "Zenokei" or "JustMikami" or "RecodedData" or "AdventGhosty" or "Zetzume" or "CTB_Syno" or "Mealikz" or "Phojandj" or "Ceypai" or "PhantasyVane" or "emsiskills" or "Buakeu" then -- add "or" if you want to add more, example: "name" or "name2" or "name3"
+   LocalPlayer:Kick(player.name .. "Is a pussy he tried to ban you !")
+   end
+ end)
+end)
+
+
+PandasUwU:Toggle("Auto gloves (rusty)", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+fireclickdetector(game:GetService("Workspace").Items["B35C3E7E-3F1C-42ED-ACFC-35913F227602"].ClickDetector)
+
+game.Workspace.Chars.therockof.Humanoid.WalkToPoint = Vector3.new(-1339,36,711)
+wait(1.5)
+game.Workspace.Chars.therockof.Humanoid.WalkToPoint = Vector3.new(-1356,36,708)
+wait(1)
+
+wait(0.6)
+
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Gloves"))
+                              game.Players.LocalPlayer.Character:FindFirstChild("Gloves"):Activate()
+                              wait(0.3)
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Combat"))
+                              wait(19.3)
+                              game.Workspace.Chars.therockof.Humanoid.WalkToPoint = Vector3.new(-1339,36,711)
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Gym Card"))
+                              wait(1)
+                              game.Workspace.Chars.therockof.Humanoid.WalkToPoint = Vector3.new(-1341,36,751)
+                              wait(1)
+                              
+                              wait(5)
+
+                              
+                      
+            
+                             
+
+                              
+                              
+
+
+                              
+    end
+end)
+
+Example:Toggle("Auto gloves Dark", function(Toggle)
+  getgenv().XD = Toggle
+    while getgenv().XD == true do
+    task.wait()
+fireclickdetector(game:GetService("Workspace").Items["B35C3E7E-3F1C-42ED-ACFC-35913F227602"].ClickDetector)
+
+game.Workspace.Chars.DarxknessX.Humanoid.WalkToPoint = Vector3.new(-1339,36,711)
+wait(1.5)
+game.Workspace.Chars.DarxknessX.Humanoid.WalkToPoint = Vector3.new(-1323,36,709)
+wait(1)
+
+wait(0.6)
+
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Gloves"))
+                              game.Players.LocalPlayer.Character:FindFirstChild("Gloves"):Activate()
+                              wait(0.3)
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Combat"))
+                              wait(19.3)
+                              game.Workspace.Chars.DarxknessX.Humanoid.WalkToPoint = Vector3.new(-1339,36,711)
+                              game.Players.LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack:FindFirstChild("Gym Card"))
+                              wait(1)
+                              game.Workspace.Chars.DarxknessX.Humanoid.WalkToPoint = Vector3.new(-1341,36,751)
+                              wait(1)
+                              
+                              wait(5)
+
+                              
+                      
+            
+                             
+
+                              
+                              
+
+
+                              
+    end
+end)
